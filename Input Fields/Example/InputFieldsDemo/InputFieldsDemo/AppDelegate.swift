b2b3ab95dfe62ae29c5e240eb18d189dafc42e77
József Vesza <jozsef.vesza@outlook.com>
2018-11-25 13:59:43 +0100

    Project setup for input fields

---
+++ Input Fields/Example/InputFieldsDemo/InputFieldsDemo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  InputFieldsDemo
+//
+//  Created by József Vesza on 2018. 11. 25..
+//  Copyright © 2018. József Vesza. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Input Fields/Example/InputFieldsDemo/InputFieldsDemo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  InputFieldsDemo
+//
+//  Created by József Vesza on 2018. 11. 25..
+//  Copyright © 2018. József Vesza. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
